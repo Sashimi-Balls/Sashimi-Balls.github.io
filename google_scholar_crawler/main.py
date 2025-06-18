ec26850447a04ca4926a6db221076e8430e86e95
SashimiBalls <141292166+Sashimi-Balls@users.noreply.github.com>
2025-06-18 19:26:41 +0800

    Add files via upload

---
+++ google_scholar_crawler/main.py
@@ -0,0 +1,43 @@
+from scholarly import scholarly, ProxyGenerator
+import logging
+from fp.fp import FreeProxy
+# import jsonpickle
+import json
+from datetime import datetime
+import os
+logging.basicConfig(level=logging.INFO)
+
+try:
+    logging.info("Script started")
+    proxy = FreeProxy().get()
+    logging.info(f"Proxy: {proxy}")
+    pg = ProxyGenerator()
+    pg.FreeProxies()
+    scholarly.use_proxy(pg)
+
+    GOOGLE_SCHOLAR_ID = 'FZvs20wAAAAJ'
+    author: dict = scholarly.search_author_id(GOOGLE_SCHOLAR_ID)
+    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+    name = author['name']
+    author['updated'] = str(datetime.now())
+    author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+except Exception as e:
+    logging.error(f"Error occurred: {e}")
+    raise
+
+os.makedirs('results', exist_ok=True)
+with open(f'results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
+
+print('Create results/gs_data.json Done')
+
+
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['citedby']}",
+}
+with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
+
+print('Create results/gs_data_shieldsio.json Done')
